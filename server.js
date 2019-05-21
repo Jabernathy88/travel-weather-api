ae60a0774bfe525fcebdb2ff23e31578420bcb68
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-21 15:26:59 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+import cors from 'cors'
+const express = require('express')
+const app = express()
+// const routes = require('./routes/index')
+
+import Sequelize from 'sequelize';
+
+// initialize db
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'postgres',
+  },
+);
+
+// test db connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Postgres connection successful')
+  })
+  // catch db error
+  .catch(err => {
+    console.error('Unable to connect database:', err)
+  })
+
+// initialize app & modules
+app.use(cors())
+
+// register routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// app listen
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`)
+})
