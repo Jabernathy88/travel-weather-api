82d81e3aa7f135075ef3d72c72c0d6398fb20879
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-21 21:29:36 -0400

    Add test route to server.js.

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 app.get('/users', async (req, res) => {
   try {
-    const users = await models.User.findAll()
+    const users = await db.models.User.findAll()
     res.json(users)
   } catch (error) {
     console.log(error)
